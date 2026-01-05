f129c6b9f5c9b63e5642f0f6327124838e0b6ce4
Jens Krumsieck <j.krumsieck@outlook.de>
2026-01-05 11:06:40 +0100

    minor improvements

--- examples/cwl_load.rs
+++ examples/cwl_load.rs
@@ -1,11 +1,9 @@
-use std::{env, fs};
-
-use commonwl::documents::CWLDocument;
+use std::env;
+use commonwl::load_cwl_file;
 
 fn main() {
     let arg = env::args().nth(1).expect("Please provide one argument");
-    let contents = fs::read_to_string(arg).unwrap();
-    let result_doc = serde_yaml::from_str::<CWLDocument>(&contents);
+    let result_doc = load_cwl_file(arg, true);
     match result_doc {
         Ok(doc) => println!("Successfully loaded document: {:#?}", doc),
         Err(e) => eprintln!("Failed to load document: {}", e),
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
-use serde::{Deserialize, Serialize};
+use std::{fs, path::Path};
 
-pub mod types;
+use serde::{Deserialize, Serialize};
+use serde_yaml::Value;
+
+use crate::documents::CWLDocument;
+
 pub mod deserialize;
-pub mod files;
-pub mod requirements;
-pub mod inputs;
 pub mod documents;
+pub mod files;
+pub mod inputs;
 pub mod outputs;
+pub mod requirements;
+pub mod types;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[serde(untagged)]
@@ -44,4 +49,49 @@ pub enum Integer {
 pub enum OneOrMany<T> {
     One(T),
     Many(Vec<T>),
+}
+
+pub fn load_cwl_file<P: AsRef<Path>>(path: P, preprocess: bool) -> anyhow::Result<CWLDocument> {
+    let contents = if preprocess {
+        preprocess_cwl_file(&path)?
+    } else {
+        fs::read_to_string(&path)?
+    };
+    serde_yaml::from_str::<CWLDocument>(&contents).map_err(|e| e.into())
+}
+
+pub fn preprocess_cwl_file<P: AsRef<Path>>(path: P) -> anyhow::Result<String> {
+    let contents = fs::read_to_string(&path)?;
+    let mut yaml: Value = serde_yaml::from_str(&contents)?;
+    let path = path.as_ref().parent().unwrap_or_else(|| Path::new("."));
+
+    resolve_imports(&mut yaml, path)?;
+    Ok(serde_yaml::to_string(&yaml)?)
+}
+
+fn resolve_imports(value: &mut Value, base_path: &Path) -> anyhow::Result<()> {
+    match value {
+        Value::Mapping(map) => {
+            if map.len() == 1
+                && let Some(Value::String(file)) = map.get(Value::String("$import".to_string()))
+            {
+                let path = base_path.join(file);
+                let contents = fs::read_to_string(&path)?;
+                let mut imported_value: Value = serde_yaml::from_str(&contents)?;
+                resolve_imports(&mut imported_value, path.parent().unwrap_or(base_path))?;
+                *value = imported_value;
+                return Ok(());
+            }
+            for val in map.values_mut() {
+                resolve_imports(val, base_path)?;
+            }
+        }
+        Value::Sequence(seq) => {
+            for val in seq.iter_mut() {
+                resolve_imports(val, base_path)?;
+            }
+        }
+        _ => {}
+    }
+    Ok(())
 }
