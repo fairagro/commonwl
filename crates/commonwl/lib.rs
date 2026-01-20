845ebb796146d593eb5b16ab276ed58b62e1eb90
Jens Krumsieck <j.krumsieck@outlook.de>
2026-01-20 12:15:35 +0100

    minor

--- crates/commonwl/lib.rs
+++ crates/commonwl/lib.rs
@@ -1,6 +1,6 @@
 pub use cwl_core::*;
 
-#[cfg(feature = "execution")]
-pub mod execution {
-    pub use cwl_execution::*;
+#[cfg(feature = "engine")]
+pub mod engine {
+    pub use cwl_engine::*;
 }
