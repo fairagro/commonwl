fbc1a11879026a83d4a610480843e2caca51bade
Jens Krumsieck <j.krumsieck@outlook.de>
2026-05-20 08:21:45 +0200

    improve docs

--- crates/commonwl/lib.rs
+++ crates/commonwl/lib.rs
@@ -1,11 +1,16 @@
+#[doc(inline)]
 pub use cwl_core::*;
 
 #[cfg(feature = "engine")]
+#[cfg_attr(docsrs, doc(cfg(feature = "engine")))]
 pub mod engine {
+    #[doc(inline)]
     pub use cwl_engine::*;
 }
 
 #[cfg(feature = "engine")]
+#[cfg_attr(docsrs, doc(cfg(feature = "engine")))]
 pub mod storage {
+    #[doc(inline)]
     pub use cwl_engine_storage::*;
 }
