168a2c8738277fcf51e2be0635e4027b8c914bd8
Jens Krumsieck <j.krumsieck@outlook.de>
2026-05-20 14:28:07 +0200

    re-enable

--- crates/engine/backend/mount.rs
+++ crates/engine/backend/mount.rs
@@ -69,10 +69,8 @@ pub(crate) fn mount_input(task: &mut Task, input: &FileOrDirectory) -> anyhow::R
     Ok(())
 }
 
-#[non_exhaustive]
 pub enum MountStrategy {
     Local,
-    #[cfg(feature = "tes")]
     Remote {
         base_url: Url,
     },
